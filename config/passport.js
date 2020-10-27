52b037b52274cbf847a0944cf906b395023e1dff
Kevin Luo <kvn.luo@gmail.com>
2020-10-26 22:20:36 -0400

    mserver.js comments middleware

--- config/passport.js
+++ config/passport.js
@@ -3,9 +3,9 @@ var LocalStrategy = require("passport-local").Strategy;
 
 var db = require("../models");
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+// Telling passport we want to use a Local Strategy to authenticate a request. In other words, we want login with a username/email and password
 passport.use(new LocalStrategy(
-  // Our user will sign in using an email, rather than a "username"
+  // Our user will sign in using an email, rather than the default parameter name 'username'
   {
     usernameField: "email"
   },
@@ -29,6 +29,7 @@ passport.use(new LocalStrategy(
         });
       }
       // If none of the above, return the user
+      // dbUser contains the user object along with several other keys
       return done(null, dbUser);
     });
   }
@@ -37,10 +38,12 @@ passport.use(new LocalStrategy(
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
 // Just consider this part boilerplate needed to make it all work
+// we get the user object which was passed from passport
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
 
+// obj is just the user object. passport.session uses deserialized obj and asigns it to req.user
 passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
--- server.js
+++ server.js
@@ -10,13 +10,22 @@ var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
+// allows server to recognize post/put request objects as strings/arrays
 app.use(express.urlencoded({ extended: true }));
+// allows server to recognize post/put request objects as JSON
 app.use(express.json());
+// allows files in 'public' directory to be accessed by the root url
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// initializes session middleware
+// only the session id is saved in the cookie. session data is saved server side
+app.use(session({ 
+  secret: "keyboard cat", // needed to sign session ID cookie
+  resave: true, // saves session back to session store even if no modifications were made
+  saveUninitialized: true // saves new and unmodified session to store
+}));
+app.use(passport.initialize()); // initialize passport
+app.use(passport.session()); // sets req.user to deserialized user object
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
